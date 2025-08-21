e0fad27ac2d94335744edcd3691e419270b172ca
kuba <meowmme@gmail.com>
2025-08-21 22:51:18 +0200

    344

---
+++ Easy/344.ReverseString.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public void ReverseString(char[] s) {
+        int l = 0;
+        int r = s.Length - 1;
+        while(l < r){
+            (s[l], s[r]) = (s[r], s[l]);
+            l++;
+            r--;
+        }
+    }
+}
